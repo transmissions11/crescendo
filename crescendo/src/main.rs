6f48b4f342b8439933c7c6942cdbb649437c5370
t11s <greenfeatherhelp@gmail.com>
2025-07-16 16:43:02 -0700

    Update main.rs

--- crescendo/src/main.rs
+++ crescendo/src/main.rs
@@ -31,6 +31,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         let handle = std::thread::spawn(move || {
             let rt = tokio::runtime::Builder::new_current_thread()
                 .enable_all()
+                .worker_threads(1)
                 .build()
                 .unwrap();
             rt.block_on(async {
