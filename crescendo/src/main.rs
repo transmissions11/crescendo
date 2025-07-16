5f997f8d8793a9824bfa7c3fac71b14e58c595fd
t11s <greenfeatherhelp@gmail.com>
2025-07-15 17:07:56 -0700

    smaller num threads

--- crescendo/src/main.rs
+++ crescendo/src/main.rs
@@ -15,7 +15,7 @@ use thousands::Separable;
 use tokio::runtime::Runtime;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let num_threads = 128;
+    let num_threads = 64;
     let connections_per_thread = 4096 / num_threads;
 
     let url = "http://127.0.0.1:8080/";
