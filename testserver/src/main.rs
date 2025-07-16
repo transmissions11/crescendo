cc2473a8678945a82e3df4649a170bf187a55e81
t11s <greenfeatherhelp@gmail.com>
2025-07-16 10:01:23 -0700

    feat: thousands

--- testserver/src/main.rs
+++ testserver/src/main.rs
@@ -4,6 +4,7 @@ use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+use thousands::Separable;
 
 #[derive(Clone)]
 struct HelloWorld {
@@ -29,7 +30,11 @@ fn main() {
             thread::sleep(Duration::from_secs(1));
             let current_count = counter_clone.load(Ordering::Relaxed);
             let rps = current_count - last_count;
-            println!("RPS: {}", rps);
+            println!(
+                "RPS: {}, Total requests: {}",
+                rps.separate_with_commas(),
+                current_count.separate_with_commas()
+            );
             last_count = current_count;
         }
     });
