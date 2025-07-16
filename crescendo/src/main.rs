8eedb990f7302443ab49d43a84a78d5054cdb0f9
t11s <greenfeatherhelp@gmail.com>
2025-07-15 17:08:54 -0700

    tune

--- crescendo/src/main.rs
+++ crescendo/src/main.rs
@@ -15,8 +15,8 @@ use thousands::Separable;
 use tokio::runtime::Runtime;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let num_threads = 64;
-    let connections_per_thread = 2048 / num_threads;
+    let num_threads = 12;
+    let connections_per_thread = 400 / num_threads;
 
     let url = "http://127.0.0.1:8080/";
 
