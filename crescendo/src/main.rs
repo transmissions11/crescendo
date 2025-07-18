437de6c0cc824db63389f5d8d2798dccb437d7d1
t11s <greenfeatherhelp@gmail.com>
2025-07-18 12:36:33 -0700

    ratio, sync again

--- crescendo/src/main.rs
+++ crescendo/src/main.rs
@@ -22,8 +22,8 @@ const TARGET_URL: &str = "http://127.0.0.1:8080";
 
 #[tokio::main(worker_threads = 1)]
 async fn main() {
-    let num_threads = num_cpus::get() as u64;
-    let connections_per_thread = TOTAL_CONNECTIONS / num_threads;
+    let mut threads_available = num_cpus::get() as u64;
+    let connections_per_thread = TOTAL_CONNECTIONS / threads_available;
 
     if let Err(err) = utils::increase_nofile_limit(TOTAL_CONNECTIONS * 10) {
         println!("Failed to increase file descriptor limit: {err}.");
@@ -31,15 +31,18 @@ async fn main() {
 
     println!(
         "Running {} threads with {} connections each against {}...",
-        num_threads, connections_per_thread, TARGET_URL
+        threads_available, connections_per_thread, TARGET_URL
     );
 
-    // Spawn all worker threads.
-    for _ in 0..(num_threads) {
+    for _ in 0..(threads_available / 3) {
+        thread::spawn(move || tx_gen::worker::tx_gen_worker());
+        threads_available -= 1;
+    }
+
+    for _ in 0..threads_available {
         thread::spawn(move || {
-            let rt = tokio::runtime::Builder::new_multi_thread().worker_threads(2).enable_all().build().unwrap();
+            let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
             rt.block_on(async {
-                tokio::spawn(tx_gen::worker::tx_gen_worker());
                 for _ in 0..connections_per_thread {
                     tokio::spawn(worker::connection_worker(TARGET_URL));
                 }
