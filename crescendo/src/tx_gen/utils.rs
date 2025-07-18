4ba2736f969974cb1da51a3b70c361616f87ba26
t11s <greenfeatherhelp@gmail.com>
2025-07-18 12:30:00 -0700

    fix async?

--- crescendo/src/tx_gen/utils.rs
+++ crescendo/src/tx_gen/utils.rs
@@ -3,7 +3,7 @@ use alloy::primitives::{Address, Bytes, TxKind, U256};
 use alloy::signers::local::PrivateKeySigner;
 use alloy_consensus::{SignableTransaction, TxLegacy};
 
-pub fn generate_and_sign_tx(
+pub async fn generate_and_sign_tx(
     signer: &PrivateKeySigner,
     chain_id: u64,
     nonce: u64,
@@ -22,12 +22,11 @@ pub fn generate_and_sign_tx(
         input: data,
     };
 
-    sign_and_encode_tx(signer, tx)
+    sign_and_encode_tx(signer, tx).await
 }
 
-pub fn sign_and_encode_tx(signer: &PrivateKeySigner, mut tx: TxLegacy) -> Vec<u8> {
-    // try async
-    let signature = signer.sign_transaction_sync(&mut tx).unwrap();
+pub async fn sign_and_encode_tx(signer: &PrivateKeySigner, mut tx: TxLegacy) -> Vec<u8> {
+    let signature = signer.sign_transaction(&mut tx).await.unwrap();
     let mut payload = Vec::new();
     tx.into_signed(signature).eip2718_encode(&mut payload);
     payload
--- crescendo/src/tx_gen/worker.rs
+++ crescendo/src/tx_gen/worker.rs
@@ -4,20 +4,15 @@ use alloy::signers::local::PrivateKeySigner;
 use crate::tx_gen::queue::PAYLOAD_QUEUE;
 use crate::tx_gen::utils::generate_and_sign_tx;
 
-pub fn tx_gen_worker() {
+pub async fn tx_gen_worker() {
     let mut nonce = 0u64;
-    let start_time = std::time::Instant::now();
 
     let signer = PrivateKeySigner::random();
 
     loop {
-        let tx = generate_and_sign_tx(&signer, 1, nonce, 10_000_000_000, 100_000, Address::from([0; 20]), Bytes::new());
+        let tx = generate_and_sign_tx(&signer, 1, nonce, 10_000_000_000, 100_000, Address::from([0; 20]), Bytes::new())
+            .await;
         PAYLOAD_QUEUE.push_payload(tx);
         nonce += 1;
-
-        if nonce == 10_000 {
-            let elapsed = start_time.elapsed();
-            println!("Time to generate 10k transactions: {:?}", elapsed);
-        }
     }
 }
