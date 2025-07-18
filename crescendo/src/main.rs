7fc9cf1e963bcc50d6890d0eb2342e44dc13eda8
t11s <greenfeatherhelp@gmail.com>
2025-07-18 12:05:35 -0700

    hm

--- crescendo/src/main.rs
+++ crescendo/src/main.rs
@@ -36,7 +36,7 @@ async fn main() {
 
     // Spawn all worker threads.
     for _ in 0..(num_threads / 2) {
-        thread::spawn(move || tx_gen::worker::tx_gen_worker());
+        // thread::spawn(move || tx_gen::worker::tx_gen_worker());
 
         thread::spawn(move || {
             let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
