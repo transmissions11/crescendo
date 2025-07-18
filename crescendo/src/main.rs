1fc8e88fa9bd84b020e0d4fe3cbf1e2987de673e
t11s <greenfeatherhelp@gmail.com>
2025-07-18 14:12:56 -0700

    bruh oops

--- crescendo/src/main.rs
+++ crescendo/src/main.rs
@@ -23,7 +23,7 @@ const TARGET_URL: &str = "http://127.0.0.1:8080";
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    let core_ids = core_affinity::get_core_ids().unwrap();
+    let mut core_ids = core_affinity::get_core_ids().unwrap();
     let mut threads_available = core_ids.len() as u64;
     let connections_per_thread = TOTAL_CONNECTIONS / threads_available as u64;
 
@@ -44,13 +44,12 @@ async fn main() {
             println!("Spawning tx gen worker on core {}", core_id.id);
             thread::spawn(move || {
                 // core_affinity::set_for_current(core_id);
-                core_affinity::set_for_current(core_id);
                 tx_gen::worker::tx_gen_worker();
             });
         } else {
             println!("Spawning connection worker on core {}", core_id.id);
             thread::spawn(move || {
-                core_affinity::set_for_current(core_id);
+                // core_affinity::set_for_current(core_id);
                 let rt = tokio::runtime::Builder::new_current_thread().enable_all().build().unwrap();
                 rt.block_on(async {
                     for _ in 0..connections_per_thread {
