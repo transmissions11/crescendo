80ef060b7d31d36863f55b378e471ac00ae2cbbf
t11s <greenfeatherhelp@gmail.com>
2025-07-18 12:19:38 -0700

    Revert "Reapply "wut""
    This reverts commit c8879ce8c34790f65816c7f084226311e7badc4d.

--- crescendo/src/tx_gen/worker.rs
+++ crescendo/src/tx_gen/worker.rs
@@ -10,15 +10,8 @@ pub fn tx_gen_worker() {
     let signer = PrivateKeySigner::random();
 
     loop {
-        let start = std::time::Instant::now();
         let tx = generate_and_sign_tx(&signer, 1, nonce, 10_000_000_000, 100_000, Address::from([0; 20]), Bytes::new());
-        let tx_gen_time = start.elapsed();
-
-        let start = std::time::Instant::now();
         PAYLOAD_QUEUE.push_payload(tx);
-        let queue_push_time = start.elapsed();
-
-        println!("TX generation time: {:?}, Queue push time: {:?}", tx_gen_time, queue_push_time);
         nonce += 1;
     }
 }
