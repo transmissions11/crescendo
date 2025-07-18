50c07c283e3d37128e7aa294cb760051128ee176
t11s <greenfeatherhelp@gmail.com>
2025-07-18 12:20:03 -0700

    Update worker.rs

--- crescendo/src/tx_gen/worker.rs
+++ crescendo/src/tx_gen/worker.rs
@@ -6,6 +6,7 @@ use crate::tx_gen::utils::generate_and_sign_tx;
 
 pub fn tx_gen_worker() {
     let mut nonce = 0u64;
+    let start_time = std::time::Instant::now();
 
     let signer = PrivateKeySigner::random();
 
@@ -13,5 +14,10 @@ pub fn tx_gen_worker() {
         let tx = generate_and_sign_tx(&signer, 1, nonce, 10_000_000_000, 100_000, Address::from([0; 20]), Bytes::new());
         PAYLOAD_QUEUE.push_payload(tx);
         nonce += 1;
+
+        if nonce == 10_000 {
+            let elapsed = start_time.elapsed();
+            println!("Time to generate 10k transactions: {:?}", elapsed);
+        }
     }
 }
