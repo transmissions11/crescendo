1b8b2bd235cc3b92e589c21740db4a82dc0a3ccd
t11s <greenfeatherhelp@gmail.com>
2025-07-18 17:26:12 -0700

    Update tx_queue.rs

--- crescendo/src/tx_queue.rs
+++ crescendo/src/tx_queue.rs
@@ -1,18 +1,19 @@
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::Mutex;
 
 use thousands::Separable;
 
 pub struct TxQueue {
     queue: Mutex<Vec<Vec<u8>>>,
+    total_added: AtomicU64,
 }
 
-pub static TX_QUEUE: TxQueue = TxQueue { queue: Mutex::new(Vec::new()) };
+pub static TX_QUEUE: TxQueue = TxQueue { queue: Mutex::new(Vec::new()), total_added: AtomicU64::new(0) };
 
 impl TxQueue {
     pub fn push_tx(&self, tx: Vec<u8>) {
-        if let Ok(mut queue) = self.queue.lock() {
-            queue.push(tx);
-        }
+        self.total_added.fetch_add(1, Ordering::Relaxed);
+        self.queue.lock().unwrap().push(tx);
     }
 
     pub fn queue_len(&self) -> usize {
@@ -26,20 +27,24 @@ impl TxQueue {
             None
         }
     }
-
     pub async fn start_reporter(&self, measurement_interval: std::time::Duration) {
+        let mut last_total_added = 0u64;
         let mut last_queue_len = 0usize;
         let mut interval = tokio::time::interval(measurement_interval);
         interval.tick().await;
         loop {
             interval.tick().await;
+            let current_total_added = self.total_added.load(Ordering::Relaxed);
             let current_queue_len = self.queue_len();
+            let added_per_second = current_total_added - last_total_added;
             let queue_growth = current_queue_len.saturating_sub(last_queue_len);
             println!(
-                "[*] TxQueue Δ/s: {}, Total length: {}",
+                "[*] TxQueue +/s: {}, TxQueue Δ/s: {}, Total length: {}",
+                added_per_second.separate_with_commas(),
                 queue_growth.separate_with_commas(),
                 current_queue_len.separate_with_commas()
             );
+            last_total_added = current_total_added;
             last_queue_len = current_queue_len;
         }
     }
