cf4bd5954b8a4c2ce14651e57c240fecd650ef10
t11s <greenfeatherhelp@gmail.com>
2025-07-21 16:55:31 -0700

    feat: even slower ramp up

--- crescendo/src/tx_queue.rs
+++ crescendo/src/tx_queue.rs
@@ -10,10 +10,8 @@ use crate::workers::NUM_ACCOUNTS;
 
 const INITIAL_RATELIMIT: u64 = 250;
 #[rustfmt::skip]
-const RATELIMIT_THRESHOLDS: [(u32, u64); 7] = [
-    (NUM_ACCOUNTS / 8,    500),
-    (NUM_ACCOUNTS / 4,    750),
-    (NUM_ACCOUNTS / 2,  1_000),
+const RATELIMIT_THRESHOLDS: [(u32, u64); 5] = [
+    (NUM_ACCOUNTS / 4,    500),
     (NUM_ACCOUNTS,      2_500),
     (NUM_ACCOUNTS * 2,  5_000),
     (NUM_ACCOUNTS * 4, 10_000),
