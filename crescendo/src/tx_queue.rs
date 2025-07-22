963441f8d9f91d779eab0ed10cd71f45f72dd35d
t11s <greenfeatherhelp@gmail.com>
2025-07-21 18:59:39 -0700

    faster!

--- crescendo/src/tx_queue.rs
+++ crescendo/src/tx_queue.rs
@@ -12,7 +12,7 @@ const INITIAL_RATELIMIT: u64 = 100;
 #[rustfmt::skip]
 const RATELIMIT_THRESHOLDS: [(u32, u64); 6] = [
     (NUM_ACCOUNTS / 16,     250),
-    (NUM_ACCOUNTS / 4,      500),
+    (NUM_ACCOUNTS / 8,      500),
     (NUM_ACCOUNTS,        1_000),
     (NUM_ACCOUNTS * 2,    2_500),
     (NUM_ACCOUNTS * 4,   10_000),
