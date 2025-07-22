42e4c7534d3b893580f72e1eea9a0e2893b60fbd
t11s <greenfeatherhelp@gmail.com>
2025-07-22 10:46:37 -0700

    15k>

--- crescendo/src/tx_queue.rs
+++ crescendo/src/tx_queue.rs
@@ -10,7 +10,7 @@ use crate::workers::NUM_ACCOUNTS;
 
 const INITIAL_RATELIMIT: u64 = 100;
 #[rustfmt::skip]
-const RATELIMIT_THRESHOLDS: [(u32, u64); 7] = [
+const RATELIMIT_THRESHOLDS: [(u32, u64); 8] = [
     (NUM_ACCOUNTS / 16,     250),
     (NUM_ACCOUNTS / 8,      500),
     (NUM_ACCOUNTS,        1_000),
@@ -18,6 +18,7 @@ const RATELIMIT_THRESHOLDS: [(u32, u64); 7] = [
     (NUM_ACCOUNTS * 4,    5_000),
     (NUM_ACCOUNTS * 8,    7_500),
     (NUM_ACCOUNTS * 10,  12_500),
+    (NUM_ACCOUNTS * 12,  15_000),
 ]; // Note: This must be sorted in ascending order of threshold!
 
 pub struct TxQueue {
